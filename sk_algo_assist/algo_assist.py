86da8a8a9a36b0f831017eb0f762133fce03dadf
GP250034 <goutham.peri@teradata.com>
2019-02-26 09:57:47 +0530

    error checking for metric and ppaccuracies argument removed

--- sk_algo_assist/algo_assist.py
+++ sk_algo_assist/algo_assist.py
@@ -23,10 +23,6 @@ class NotPandasDataFrame(Exception):
     pass
 
 
-class PPAccuraciesNotDefined(Exception):
-    pass
-
-
 class NotPandasSeries(Exception):
     pass
 
@@ -34,15 +30,7 @@ class MetricNotDefined(Exception):
     pass
 
 
-def entry(df, y, split = 0.7, reg_or_class = '', metric = None, pp_accuracies = False):
-
-    df_y = df[y]
-    x = df.drop([y], axis=1)
-    x_train, x_test, y_train, y_test = train_test_split(x, df_y, test_size=1 - split, random_state=42)
-    compare_algos(x_train, x_test, y_train, y_test, reg_or_class, metric, pp_accuracies=True)
-
-
-def compare_algos(x_train, x_test, y_train, y_test, reg_or_class, metric, pp_accuracies):
+def compare_algos(df, y, split = 0.7, reg_or_class = '', metric = None):
     '''
 
     A function compare the algorithms that are suitable for the dataset and get the best algorithm
@@ -50,36 +38,47 @@ def compare_algos(x_train, x_test, y_train, y_test, reg_or_class, metric, pp_acc
 
     Parameters
     ----------
-    x_train:Pandas Dataframe, Required
-        The features of the training set.
+    df: Pandas Dataframe, Required
+        - Dataframe with the whole dataset, splitting is done by default or as specified by you.
+    
+    Y: String, Required
+        - The name of the column on which prediction is being done.
 
-    x_test:Pandas Dataframe, Required
-        The features of the testing set.
-
-    y_train:Pandas Dataframe, Required
-        The label of the training set.
-
-    y_test:Pandas Dataframe, Required
-        The label of the testing set.
-
-    pp_accuracies: Boolean, Optional(default = False)
+    split: Float, Optinal(default = 0.7)
+        - Values between 0 and 1(exclusive) are only allowed.
+    
+    metric: sklearn.metrics object(default=accuracy_score(classification) or mean_absolute_error(regression))
+        - Metrics Allowed:
+            * Regression     : mean_absolute_error, mean_squared_error, explained_variance_score
+            * Classification : f1_score, precision_score, recall_score, accuracy_score
+        - Eg: compare_algos(df, y, metric = f1_score)
 
     reg_or_class: String, Optional(default = '')
         - If '' then the algorithm checks for the distribution in the y_train using the formula: distribution = len(y_train.unique())/len(y_train) * 100. If the distribution is less than 10 classification algorithms are run, else regression algorithms are run.
         - If String the allowed values are "Regression" or "Classification".
 
-    :return:String
+    return:String
         The name of the algorithm that obatined the highest accuracy, along with the accuracy and time taken for fitting.
 
     '''
 
-    do_error_checking(x_train, x_test, y_train, y_test, pp_accuracies, reg_or_class, metric)
+    df_y = df[y]
+    x = df.drop([y], axis=1)
+    x_train, x_test, y_train, y_test = train_test_split(x, df_y, test_size=1 - split, random_state=42)
+    compare_algos_helper(x_train, x_test, y_train, y_test, reg_or_class, metric)
+
+
+def compare_algos_helper(x_train, x_test, y_train, y_test, reg_or_class, metric):
+
+    do_error_checking(x_train, x_test, y_train, y_test, reg_or_class, metric)
 
     algos_to_be_compared, algo_type = get_algos_to_be_compared(y_train, reg_or_class)
+
     accuracies = []
     index = 0
     total_time = 0
     times_taken = []
+
     for algo in algos_to_be_compared:
         start_time = time.time()
         print(algo, type(algo))
@@ -96,16 +95,12 @@ def compare_algos(x_train, x_test, y_train, y_test, reg_or_class, metric, pp_acc
         total_time += time_taken
         index += 1
 
-    if pp_accuracies:
-        pp_accuracies_helper(accuracies, algos_to_be_compared)
-
+    pp_accuracies_helper(accuracies, algos_to_be_compared)
     best_accuracy_index = accuracies.index(max(accuracies))
 
-    charts_data = get_chart_data(algos_to_be_compared, accuracies, times_taken, algo_type)
-    return charts_data
 
 
-def do_error_checking(x_train, x_test, y_train, y_test, pp_accuracies, reg_or_class, metric):
+def do_error_checking(x_train, x_test, y_train, y_test, reg_or_class, metric):
     if not (isinstance(x_train, pd.DataFrame) and
             isinstance(x_test, pd.DataFrame)
     ):
@@ -115,9 +110,6 @@ def do_error_checking(x_train, x_test, y_train, y_test, pp_accuracies, reg_or_cl
             isinstance(y_test, pd.Series)
     ):
         raise NotPandasSeries("The y_train, y_test should be a Pandas Series objects.")
-
-    if not isinstance(pp_accuracies, bool):
-        raise PPAccuraciesNotDefined("pp_accuracies should be True or False.")
 
     if (reg_or_class != REGRESSION and
             reg_or_class != CLASSIFICATION and
@@ -143,6 +135,14 @@ def get_distribution(y_train):
 
 
 def get_accuracy(y_test, y_pred, metric, algo_type):
+    # Error Checking
+    if metric:
+        if algo_type == REGRESSION and metric not in REGRESSION_METRICS:
+            raise MetricNotDefined("The given metric is not a regression metric")
+        if algo_type == CLASSIFICATION and metric not in CLASSIFICATION_METRICS:
+            raise MetricNotDefined("The given metric is not a classification metric")
+    # Error Checking Done
+
     accuracy = 0
 
     if algo_type == REGRESSION:
