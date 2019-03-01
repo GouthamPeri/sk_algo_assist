4e58a96b2ba39b0b9f6e53fc22ce427f793b8ba2
GP250034 <goutham.peri@teradata.com>
2019-03-01 12:35:58 +0530

    added algos_to_be_compared

--- sk_algo_assist/algo_assist.py
+++ sk_algo_assist/algo_assist.py
@@ -1,20 +1,32 @@
-from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier, RandomForestRegressor, ExtraTreesRegressor
-from sklearn.linear_model import LogisticRegression, LinearRegression
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import f1_score, precision_score, recall_score, accuracy_score
-from sklearn.metrics import mean_absolute_error, mean_squared_error, explained_variance_score
 import time
 import pandas as pd
 
+from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier, RandomForestRegressor, ExtraTreesRegressor
+from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
+from sklearn.ensemble import AdaBoostClassifier, AdaBoostRegressor, GradientBoostingClassifier, GradientBoostingRegressor
+from sklearn.linear_model import LogisticRegression, LinearRegression, SGDClassifier, SGDRegressor
+from sklearn.neighbors import KNeighborsClassifier, KNeighborsRegressor
+from sklearn.naive_bayes import GaussianNB
 
-REGRESSION_ALGOS = [LinearRegression, RandomForestRegressor, ExtraTreesRegressor]
-CLASSIFICATION_ALGOS = [RandomForestClassifier, ExtraTreesClassifier, LogisticRegression]
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import f1_score, precision_score, recall_score, accuracy_score
+from sklearn.metrics import mean_absolute_error, mean_squared_error, explained_variance_score
+from sklearn.svm import SVC, SVR
+
+
+REGRESSION_ALGOS = [LinearRegression, RandomForestRegressor, ExtraTreesRegressor, DecisionTreeRegressor, SGDRegressor, SVR, GradientBoostingRegressor, KNeighborsRegressor, AdaBoostRegressor]
+CLASSIFICATION_ALGOS = [RandomForestClassifier, ExtraTreesClassifier, LogisticRegression, DecisionTreeClassifier, SGDClassifier, SVC, GradientBoostingClassifier, KNeighborsClassifier, GaussianNB, AdaBoostClassifier]
+
+
 REGRESSION_METRICS = [mean_absolute_error, mean_squared_error, explained_variance_score]
 CLASSIFICATION_METRICS = [f1_score, precision_score, recall_score, accuracy_score]
+
 REGRESSION = "Reg"
 CLASSIFICATION = "Cla"
+
 DEFAULT_REGRESSION_METRIC = mean_absolute_error
 DEFAULT_CLASSIFICATION_METRIC = accuracy_score
+
 
 
 class RegOrClassNotDefined(Exception):
@@ -31,8 +43,11 @@ class NotPandasSeries(Exception):
 class MetricNotDefined(Exception):
     pass
 
+class AlgoToBeComparedNotDefined(Exception):
+    pass
 
-def compare_algos(df, y, split = 0.7, reg_or_class = '', metric = None):
+
+def compare_algos(df, y, split = 0.7, reg_or_class = '', metric = None, algos_to_be_compared = []):
     '''
 
     A function compare the algorithms that are suitable for the dataset and get the best algorithm
@@ -67,14 +82,15 @@ def compare_algos(df, y, split = 0.7, reg_or_class = '', metric = None):
     df_y = df[y]
     x = df.drop([y], axis=1)
     x_train, x_test, y_train, y_test = train_test_split(x, df_y, test_size=1 - split, random_state=42)
-    compare_algos_helper(x_train, x_test, y_train, y_test, reg_or_class, metric)
+    compare_algos_helper(x_train, x_test, y_train, y_test, reg_or_class, metric,algos_to_be_compared)
 
 
-def compare_algos_helper(x_train, x_test, y_train, y_test, reg_or_class, metric):
+def compare_algos_helper(x_train, x_test, y_train, y_test, reg_or_class, metric, algos_to_be_compared):
 
-    do_error_checking(x_train, x_test, y_train, y_test, reg_or_class, metric)
+    do_error_checking(x_train, x_test, y_train, y_test, reg_or_class, metric, algos_to_be_compared)
 
-    algos_to_be_compared, algo_type = get_algos_to_be_compared(y_train, reg_or_class)
+    algos_to_be_compared, algo_type = get_algos_to_be_compared(y_train, reg_or_class, algos_to_be_compared)
+    metric = get_metric(algo_type, metric)
 
     accuracies = []
     index = 0
@@ -85,9 +101,10 @@ def compare_algos_helper(x_train, x_test, y_train, y_test, reg_or_class, metric)
         start_time = time.time()
         model = algo()
         model.fit(x_train, y_train)
+        print(algo.__name__," has completed training")
         y_pred = model.predict(x_test)
 
-        accuracy = get_accuracy(y_test, y_pred, metric, algo_type)
+        accuracy = get_accuracy(y_test, y_pred, metric)
 
         accuracies.append(accuracy)
         end_time = time.time()
@@ -96,10 +113,10 @@ def compare_algos_helper(x_train, x_test, y_train, y_test, reg_or_class, metric)
         total_time += time_taken
         index += 1
 
-    pp_accuracies(accuracies, algos_to_be_compared, algo_type)
+    pp_accuracies(algos_to_be_compared, algo_type, metric, accuracies, times_taken)
 
 
-def do_error_checking(x_train, x_test, y_train, y_test, reg_or_class, metric):
+def do_error_checking(x_train, x_test, y_train, y_test, reg_or_class, metric, algos_to_be_compared):
     if not (isinstance(x_train, pd.DataFrame) and
             isinstance(x_test, pd.DataFrame)
     ):
@@ -124,6 +141,20 @@ def do_error_checking(x_train, x_test, y_train, y_test, reg_or_class, metric):
     ):
         raise MetricNotDefined(metric.__name__ + " metric is not included")
 
+    regression_algos_set = set(list(map(lambda x:x.__name__, REGRESSION_ALGOS)))
+    classification_algos_set = set(list(map(lambda x:x.__name__,CLASSIFICATION_ALGOS)))
+    algos_to_be_compared_set = set(list(map(lambda x:x.__name__,algos_to_be_compared)))
+
+    if not (algos_to_be_compared_set.issubset(regression_algos_set) or
+            algos_to_be_compared_set.issubset(classification_algos_set)
+    ):
+        if reg_or_class == REGRESSION:
+            print("The regression algorithms allowed are:", REGRESSION_ALGOS)
+        else:
+            print("The classification algorithms allowed are:", CLASSIFICATION_ALGOS)
+
+        raise AlgoToBeComparedNotDefined("One or more algorithm(s) in algos_to_be_compared list is not defined")
+
 
 def get_distribution(y_train):
     unique_values = len(y_train.unique())
@@ -132,8 +163,7 @@ def get_distribution(y_train):
 
     return distribution
 
-
-def get_accuracy(y_test, y_pred, metric, algo_type):
+def get_metric(algo_type, metric):
     # Error Checking
     if metric:
         if algo_type == REGRESSION and metric not in REGRESSION_METRICS:
@@ -149,42 +179,48 @@ def get_accuracy(y_test, y_pred, metric, algo_type):
         elif algo_type == CLASSIFICATION:
             metric = DEFAULT_CLASSIFICATION_METRIC
     
-    accuracy = 0
+    return metric
 
-    if algo_type == REGRESSION:
-        accuracy = metric(y_test, y_pred)
-    elif algo_type == CLASSIFICATION:
-        accuracy = metric(y_test, y_pred)
-    else:
-        raise RegOrClassNotDefined("reg_or_class should be either 'Reg' or 'Cla' or empty.")
+def get_accuracy(y_test, y_pred, metric):
+    
+    accuracy = 0
+    accuracy = metric(y_test, y_pred)
     return accuracy
 
 
-def get_algos_to_be_compared(y_train, reg_or_class):
-    algos_to_be_compared = []
+def get_algos_to_be_compared(y_train, reg_or_class, algos_to_be_compared):
     algo_type = ''
 
-    if reg_or_class:
-        if reg_or_class == REGRESSION:
-            algos_to_be_compared = REGRESSION_ALGOS
+    if algos_to_be_compared:
+        if algos_to_be_compared[0] in REGRESSION_ALGOS:
             algo_type = REGRESSION
-        elif reg_or_class == CLASSIFICATION:
-            algos_to_be_compared = CLASSIFICATION_ALGOS
-            algo_type = CLASSIFICATION
-    else:
-        distribution = get_distribution(y_train)
-
-        if distribution < 10:
-            algos_to_be_compared = CLASSIFICATION_ALGOS
-            algo_type = CLASSIFICATION
         else:
-            algos_to_be_compared = REGRESSION_ALGOS
-            algo_type = REGRESSION
+            algo_type = CLASSIFICATION
 
-    return algos_to_be_compared, algo_type
+        return algos_to_be_compared, algo_type
+    
+    else:
+        if reg_or_class:
+            if reg_or_class == REGRESSION:
+                algos_to_be_compared = REGRESSION_ALGOS
+                algo_type = REGRESSION
+            elif reg_or_class == CLASSIFICATION:
+                algos_to_be_compared = CLASSIFICATION_ALGOS
+                algo_type = CLASSIFICATION
+        else:
+            distribution = get_distribution(y_train)
+
+            if distribution < 10:
+                algos_to_be_compared = CLASSIFICATION_ALGOS
+                algo_type = CLASSIFICATION
+            else:
+                algos_to_be_compared = REGRESSION_ALGOS
+                algo_type = REGRESSION
+
+        return algos_to_be_compared, algo_type
 
 
-def pp_accuracies(accuracies, algos_to_be_compared, algo_type):
+def pp_accuracies(algos_to_be_compared, algo_type, metric, accuracies, times_taken):
     d = {}
     header = ['Algorithm Name']
 
@@ -192,19 +228,21 @@ def pp_accuracies(accuracies, algos_to_be_compared, algo_type):
         d[algos_to_be_compared[i].__name__] = accuracies[i]
 
     if algo_type == REGRESSION:
-        header.append('Error')
+        header.append(metric.__name__)
         d = dict(sorted(d.items(), key = lambda x : x[1]))
     else:
-        header.append('Accuracy')
+        header.append(metric.__name__)
         d = dict(sorted(d.items(), key = lambda x : x [1], reverse = True))
 
+    header.append('Time Taken')
     data = []
     index = []
     j = 1
 
     for i in d:
-        data.append([i, round(d[i], 4)])
+        data.append([i, round(d[i], 4), round(times_taken[j-1], 4)])
         index.append(j)
         j += 1
+
     pp_df = pd.DataFrame(data, index, header)
     print(pp_df)
